2f3e5a0b2b9478f41f405a72b2953ca6160e9fe1
ravi4all <ravikanttyagi.2295@gmail.com>
2018-04-19 13:39:21 +0530

    Add files via upload

---
+++ Section_4/ObjectDetection/02-CamDetect.py
@@ -0,0 +1,29 @@
+import cv2
+
+dataset = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+capture = cv2.VideoCapture(0)
+
+while True:
+    # ret -> True/False
+    # if camera works properly then true will come
+    ret, img = capture.read()
+    if ret:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = dataset.detectMultiScale(gray)
+
+        for x,y,w,h in faces:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0), 5)
+
+        cv2.imshow('img',img)
+        print(gray)
+
+    else:
+        print("Problem with camera")
+
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
