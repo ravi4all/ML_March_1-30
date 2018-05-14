be6fd68b4d5afd6d76644b48260a48970cdab8cb
ravi4all <ravikanttyagi.2295@gmail.com>
2018-05-14 18:07:47 +0530

    Add files via upload

--- Section_7/DecisionTreePython.py
+++ Section_7/DecisionTreePython.py
@@ -60,28 +60,109 @@ def evaluate_algorithm(dataset,n_folds,algorithm):
     
     return scores
 
-def  train_test():
-    pass
+def test_split(index,value,dataset):
+    left, right = list(), list()    
+    for row in dataset:
+        if row[index] < value:
+            left.append(row)
+        else:
+            right.append(row)    
+    return left, right
 
-def gini_index(class_values, group):
-    gini = 0.0
-    
+def gini_index(class_values, groups):
+    gini = 0.0    
     for class_value in class_values:
-        p = count(class_value) / len(group)
-        gini = sum(p * (1.0 - p))
+        for group in groups:        
+            size = len(group)
+            
+            if size == 0:
+                continue
+            
+            p = [row[-1] for row in group].count(class_value) / float(size)
+            gini += p * (1.0 - p)
     
-    return gini
+    return gini    
+
+def get_split(dataset):
+    
+    class_value = list(set([row[-1] for row in dataset]))
+    b_score,b_value,b_index,b_group = 999,999,999,None
+    
+    for index in range(len(dataset[0]) - 1):
+        for row in dataset:
+            groups = test_split(index,row[index],dataset)
+            gini = gini_index(class_value,groups)
+            
+        if gini < b_score:
+            b_index, b_value, b_score, b_group = index, row[index], gini, groups
+        
+    return {'score':b_score, 'value':b_value, 'index':b_index, 'groups':b_group} 
+    
+    
+def to_terminal(group):
+    outcome = [row[-1] for row in group]
+    return max(set(outcome),key=outcome.count)
+
+def split(depth,node,max_depth,min_size):
+    
+    left, right = node['groups']
+    del node['groups']
+    
+    if not left or not right:
+        node['left'] = node['right'] = to_terminal(left + right)
+        return
+    
+    if depth > max_depth:
+        node['left'], node['right'] = to_terminal(left), to_terminal(right)
+        return
+    
+    if len(left) <= min_size:
+        node['left'] = to_terminal(left)
+    else:
+        node['left'] = get_split(left)
+        split(depth+1,node['left'],max_depth,min_size)
+    
+    if len(right) <= min_size:
+        node['right'] = to_terminal(right)
+    else:
+        node['right'] = get_split(right)
+        split(depth+1,node['right'],max_depth,min_size)
     
 
-def get_split():
-    pass
-
-def decision_tree():
-    pass
+def build_tree(train,max_depth,min_size):
+    root = get_split(train)
+    split(1,root,max_depth,min_size)
+    return root
 
 
 def predict():
     pass
+
+def random_forest():
+    pass
+
+def bagging_predict():
+    pass
+
+
+
+dataset = [
+        [2.7810836,2.550537003,0],
+    	[1.465489372,2.362125076,0],
+    	[3.396561688,4.400293529,0],
+    	[1.38807019,1.850220317,0],
+    	[3.06407232,3.005305973,0],
+    	[7.627531214,2.759262235,1],
+    	[5.332441248,2.088626775,1],
+    	[6.922596716,1.77106367,1],
+    	[8.675418651,-0.242068655,1],
+    	[7.673756466,3.508563011,1]
+    ]
+
+max_depth = 10
+min_size = 1
+
+root_node = build_tree(dataset,max_depth,min_size)
 
 filename = 'german_credit.csv'
 dataset = load_data(filename)
